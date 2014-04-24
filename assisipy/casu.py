5283f41f69fbc354886bef328decfb60da7cccc4
Damjan Miklic <damjan.miklic@gmail.com>
2014-04-24 14:39:34 +0200

    Added logging and thread stopping to the Casu API.

--- assisipy/casu.py
+++ assisipy/casu.py
@@ -8,7 +8,11 @@ import time
 
 import zmq
 
-import yaml
+import yaml # For parsing configuration (.rtc) files
+
+# For logging
+from datetime import datetime
+import csv  
 
 from msg import dev_msgs_pb2
 from msg import base_msgs_pb2
@@ -60,9 +64,10 @@ class Casu:
 
     :param string rtc_file_name: Name of the run-time configuration (RTC) file. If no file is provided, the default configuration is used.
     :param string name: Casu name (if a RTC file is provided, this value is overridden).
+    :param bool log: A variable indicating whether to log all incoming and outgoing data. If set to true, a logfile in the form 'YYYY-MM-DD-HH-MM-SS-name.csv' is created.
     """
     
-    def __init__(self, rtc_file_name='', name = 'Casu'):
+    def __init__(self, rtc_file_name='', name = 'Casu', log = False):
         
         if rtc_file_name:
             # Parse the rtc file
@@ -82,6 +87,8 @@ class Casu:
             self.__msg_pub_addr = None
             self.__msg_sub = None
 
+        self.__stop = False
+
         # TODO: Fill range_readings with fake data
         #       to prevent program crashes.
         self.__ir_range_readings = dev_msgs_pb2.RangeArray()
@@ -97,6 +104,14 @@ class Casu:
         self.__comm_thread.daemon = True
         self.__lock =threading.Lock()
 
+        # Set up logging
+        self.__log = log
+        if log:
+            now_str = datetime.now().__str__().split('.')[0]
+            now_str = now_str.replace(' ','-').replace(':','-')
+            self.__logfile = open(now_str + '-' + self.__name + '.csv','wb')
+            self.__logger = csv.writer(self.__logfile,delimiter=';')
+
         # Create inter-casu communication sockets
         self.__msg_queue = []
         if self.__msg_pub_addr:
@@ -107,11 +122,11 @@ class Casu:
                 self.__msg_sub.connect(self.__neighbors[direction]['address'])
             self.__msg_sub.setsockopt(zmq.SUBSCRIBE, self.__name)
         
-        
         # Connect the control publisher socket
         self.__pub = self.__context.socket(zmq.PUB)
         self.__pub.connect(self.__pub_addr)
         
+        # Connect to the device and start receiving data
         self.__comm_thread.start()
         # Wait for the connection
         while not self.__connected:
@@ -129,7 +144,7 @@ class Casu:
         self.__sub.connect(self.__sub_addr)
         self.__sub.setsockopt(zmq.SUBSCRIBE, self.__name)
         
-        while True:
+        while not self.__stop:
             [name, dev, cmd, data] = self.__sub.recv_multipart()
             self.__connected = True
             if dev == 'IR':
@@ -138,6 +153,7 @@ class Casu:
                     # to make sure all data is written before access
                     with self.__lock:
                         self.__ir_range_readings.ParseFromString(data)
+                    self.__write_to_log([time.time()] + [r for r in self.__ir_range_readings.range])
                 else:
                     print('Unknown command {0} for {1}'.format(ranges, self.__name))
             else:
@@ -152,6 +168,25 @@ class Casu:
                 except zmq.ZMQError:
                     # Nobody is sending us a message. No biggie.
                     pass
+
+    def __cleanup(self):
+        """
+        Performs necessary cleanup operations, i.e. stops communication threads,
+        closes connections and files.
+        """        
+        # Wait for communicaton threads to finish
+        self.__comm_thread.join()
+
+        if self.__log:
+            self.__logfile.close()
+
+    def stop(self):
+        """
+        Stops the Casu interface and cleans up.
+        """
+        self.__stop = True
+        self.__cleanup()
+
 
     def get_range(self, id):
         """ 
@@ -332,12 +367,19 @@ class Casu:
         
         return msg
 
+    def __write_to_log(self, data):
+        """
+        Write one line of data to the logfile.
+        """
+        if self.__log:
+            self.__logger.writerow(data)
 
 if __name__ == '__main__':
     
     casu1 = Casu()
     switch = -1
-    while True:
+    count = 0
+    while count < 10:
         print(casu1.get_range(IR_N))
         if switch > 0:
             casu1.diagnostic_led_standby(DLED_TOP)
@@ -345,3 +387,6 @@ if __name__ == '__main__':
             casu1.set_diagnostic_led_rgb(DLED_TOP, 1, 0, 0)
         switch *= -1
         time.sleep(1)
+        count = count + 1
+
+    casu1.stop()

3e9bfc2f23e45a811fe17c0598bfd29c7f4fc50b
Damjan Miklic <damjan.miklic@gmail.com>
2014-09-14 21:11:11 +0200

    Example combining stimulus-based and communication-based CASU interaction seems to be working.

--- examples/workshop_graz_2014/comms_demo/casu-001/casu_heater.py
+++ examples/workshop_graz_2014/comms_demo/casu-001/casu_heater.py
@@ -7,14 +7,25 @@ from assisipy import casu
 
 
 heating_temp = 40
-cooling_temp = 15
+cooling_temp = 25
 
 if __name__ == '__main__':
     
     heatcasu = casu.Casu()
-    
+    temp = heating_temp
+    heatcasu.set_diagnostic_led_rgb(r=1)
+
     while True:
-        heatcasu.set_temp(heating_temp)
+        msg = heatcasu.read_message()
+        if msg:
+            if 'Warm' in msg['data']:
+                temp = heating_temp
+                heatcasu.set_diagnostic_led_rgb(r=1)
+            elif 'Cool' in msg['data']:
+                temp = cooling_temp
+                heatcasu.set_diagnostic_led_rgb(b=1)
+
+        heatcasu.set_temp(temp)
         sleep(5)
 
 

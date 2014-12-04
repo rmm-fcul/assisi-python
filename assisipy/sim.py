1fbc4c3883601f95be69e0808511db47c983c252
Rob Mills <rob.mills@zepler.net>
2014-12-04 14:18:56 +0000

    introduced a kwarg for pub_addr in sim Control.
    - enables explicit selection of which instance of Enki to control
    - also included explicit error if an RTC file is passed in

--- assisipy/sim.py
+++ assisipy/sim.py
@@ -13,45 +13,51 @@ from msg import base_msgs_pb2
 from msg import dev_msgs_pb2
 
 class Control:
-    """ 
-    Simulator control API. 
+    """
+    Simulator control API.
 
-    Creates a command publisher and connects it to the simulator. 
-        
+    Creates a command publisher and connects it to the simulator.
+
     :param string rtc_file_name: Name of the run-time configuraiton file. This file specifies the parameters for connecting to the simulator.
+
+    :param dict kwargs: accepts a string to override value for `pub_addr`
 
     """
 
-    def __init__(self, rtc_file_name=''):
+    def __init__(self, rtc_file_name='', **kwargs):
 
         if rtc_file_name:
             # Parse the rtc file
+            raise NotImplementedError(
+                "RTC file parsing for Bees is not implemented yet. "
+                "Please call the constructor with the name=beename argument.")
             pass
         else:
-            # Use default values
-            self.__pub_addr = 'tcp://127.0.0.1:5556'
+            # parse any keywords provided, otherwise take default values
+            self.__pub_addr = kwargs.get('pub_addr', 'tcp://127.0.0.1:5556')
+            #self.__pub_addr = 'tcp://127.0.0.1:5556'
             self.__context = zmq.Context(1)
             self.__pub = self.__context.socket(zmq.PUB)
             self.__pub.connect(self.__pub_addr)
-            
-    def spawn(self, 
-              obj_type, 
-              name, 
-              pose, 
-              polygon = (), 
-              radius = (), 
+
+    def spawn(self,
+              obj_type,
+              name,
+              pose,
+              polygon = (),
+              radius = (),
               color = (),
               height = 1,
               mass = -1):
-        """ 
+        """
 
         Spawn an object in the simulated world.
-            
+
         :param str obj_type: Type of object to spawn. Currently supported types are 'Casu', 'Bee', 'EPuck' and 'Physical'
         :param str name: Name of the object. Must be unique in the world.
         :param tuple pose: An (x,y,yaw) tuple.
         :param tuple polygon: A tuple of vertex coordinates ((x1,y1),(x2,y2),...). If obj_type is 'Physical', this defines the shape of the object.
-        :param radius: Radius of a cylindrical 'Physical' object. 
+        :param radius: Radius of a cylindrical 'Physical' object.
         :param color: Color of a 'Physical' object.
         :param float height: 'Physical' object height.
         :param mass: 'Physical' object mass.
@@ -81,7 +87,7 @@ class Control:
             data.polygon.height = height
             data.polygon.mass = mass
             data.type = 'Polygon'
-        self.__pub.send_multipart(['Sim', 'Spawn', obj_type, 
+        self.__pub.send_multipart(['Sim', 'Spawn', obj_type,
                                    data.SerializeToString()])
 
     def teleport(self, obj_name, pose):

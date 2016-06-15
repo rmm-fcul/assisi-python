a26a9fa66cd038c93a33594e2aa0589696303751
Rob Mills <rob.mills@zepler.net>
2016-06-15 11:31:54 +0100

    implemented the main deployment generation

--- assisipy/prep_calib.py
+++ assisipy/prep_calib.py
@@ -33,6 +33,7 @@ class PrepCalib(object):
 
         self.project_root = os.path.dirname(os.path.abspath(project_file_name))
         self.sandbox_dir = self.proj_name + '_calib' + '_sandbox'
+        self.main_sandbox_dir = self.proj_name + '_main' + '_sandbox'
 
 
         self.prepared = False
@@ -51,13 +52,107 @@ class PrepCalib(object):
         self.out_project_file = "calib_" + self.proj_name + '.assisi'
         self.out_dep_file     = "calib_" + self.project['dep']
 
+        self.main_out_dep_file     = "main_" + self.project['dep']
+        self.main_out_project_file = "main_" + self.proj_name + '.assisi'
 
     def prep(self):
         self.prep_calib()
         self.prep_main()
 
     def prep_main(self):
-        pass
+        '''
+        prepare the main code in local folder
+        '''
+        cwd = os.getcwd()
+        print('Changing directory to {0}'.format(self.project_root))
+        os.chdir(self.project_root)
+        print('Preparing main deployment config!')
+
+        # Clean up and create new sandbox folder
+        sandbox_path = os.path.join(self.project_root, self.main_sandbox_dir)
+        _msg = "created"
+        if os.path.exists(sandbox_path):
+            _msg = "overwritten"
+        print("The folder \n\t{}\n will be {}".format(sandbox_path, _msg))
+        try:
+            shutil.rmtree(sandbox_path)
+        except OSError:
+            # The sandbox directory does not exist, no biggie
+            pass
+
+        os.mkdir(self.main_sandbox_dir)
+        os.chdir(self.main_sandbox_dir)
+        # construct a new deployment file, with entries for all CASUs in
+        # original dep, and with added args and extra support files for those
+        # CASUs with a calibration entry
+        main_dep = {}
+        for layer in self.arena:
+            main_dep[layer] = {}
+            for casu in self.arena[layer]:
+                # find the basic deployment info
+                depdata = self.dep[layer][casu]
+                maindepinfo = {}
+                # required
+                for k in [ 'hostname', 'user', 'prefix', 'controller']:
+                    maindepinfo[k] = depdata[k] # fail if not defined
+
+                # optional, don't fail if undef
+                for k in [ 'args', 'extra', 'results']:
+                    v = depdata.get(k, []) # assign empty list by default
+                    maindepinfo[k] = v
+
+                # and more proc if there is present a non-empty calibration subdict,
+                calibdata = depdata.get('calibration')
+                #print casu, calibdata
+                if calibdata is None:
+                    # nothing more to do
+                    print "[I] skipping extra calib proc for {} {}".format(layer, casu)
+                    main_dep[layer][casu] = maindepinfo
+                    continue
+
+                # output file of the calibration will be in the location
+                # <proj>_data/<layer>/<casu>/XXX
+                #TODO: custom --logpath location...
+                calibfile = calibdata['calibfile']
+                calib_data_dir = os.path.join(self.project_root,
+                                              self.sandbox_dir,
+                                              'data_' + self.proj_name,
+                                              layer,
+                                              casu)
+                maindepinfo['extra'].append(os.path.join(calib_data_dir, calibfile))
+                # no path needed on the CASU side. (files tx to controller wd)
+                maindepinfo['args'].append("--calibfile {}".format(calibfile))
+
+                print "[I] constructed augmented MAIN deployment info for {}".format(casu)
+                print maindepinfo
+
+
+                main_dep[layer][casu] = maindepinfo
+
+        sd = yaml.safe_dump(main_dep, default_flow_style=False)
+        with open(self.main_out_dep_file, 'w')  as outdep:
+            outdep.write(sd + "\n")
+
+        # now do the project file - just update the .dep file
+        mainproj = dict(self.project)
+        mainproj['dep'] = self.main_out_dep_file
+
+        sp = yaml.safe_dump(mainproj, default_flow_style=False)
+        with open(self.main_out_project_file, 'w')  as outproj:
+            outproj.write(sp + "\n")
+
+
+        # finally, copy the .arena and .nbg files to the sandbox.
+        dst = os.path.join(self.project_root, self.main_sandbox_dir)
+        src = os.path.join(self.project_root, mainproj['arena'])
+        #print "src to dest: \n\t{} \n\t{}".format(src, dst)
+        shutil.copy2(src, dst)
+        src = os.path.join(self.project_root, mainproj['nbg'])
+        #print "src to dest: \n\t{} \n\t{}".format(src, dst)
+        shutil.copy2(src, dst)
+
+        print('Returning to original directory {0}'.format(cwd))
+        os.chdir(cwd)
 
     def prep_calib(self):
         '''
@@ -66,7 +161,7 @@ class PrepCalib(object):
         cwd = os.getcwd()
         print('Changing directory to {0}'.format(self.project_root))
         os.chdir(self.project_root)
-        print('Preparing !')
+        print('Preparing calibration deployment config!')
 
         # Clean up and create new sandbox folder
         sandbox_path = os.path.join(self.project_root, self.sandbox_dir)
@@ -77,8 +172,7 @@ class PrepCalib(object):
         try:
             shutil.rmtree(sandbox_path)
         except OSError:
-            # The sandbox directory does not exist,
-            # no biggie
+            # The sandbox directory does not exist, no biggie
             pass
 
         os.mkdir(self.sandbox_dir)
@@ -108,7 +202,7 @@ class PrepCalib(object):
                 calibfile = calibdata['calibfile']
 
                 calibdepinfo['controller'] = calibdata['controller']
-                calibdepinfo['output']     = calibdata.get('output', []) + [calibfile]
+                calibdepinfo['results']     = calibdata.get('results', []) + [calibfile]
                 calibdepinfo['extra']      = calibdata.get('extra', [])  + ["--output", calibfile]
 
                 print "[I] constructed new deployment info for {}".format(casu)
